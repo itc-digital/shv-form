d86203601eb565bd0ced084584293f05764bd992
Dmitry Lekomtsev <dolf99@yandex.ru>
2018-03-15 21:46:37 +0400

    Добавил по аналогии поля

--- src/App.js
+++ src/App.js
@@ -19,194 +19,292 @@ import Disclamer from './Disclamer';
 import { Component } from 'inferno';
 
 class App extends Component {
-  render() {
-    const { values, errors, handleBlur, handleChange, handleSubmit } = this.props;
+    render() {
+        const {
+            values,
+            errors,
+            handleBlur,
+            handleChange,
+            handleSubmit
+        } = this.props;
 
-    return (
-      <RootContainer>
-        <FormCard>
-          <FormCardImage>
-            <ScrollHint />
-          </FormCardImage>
+        return (
+            <RootContainer>
+                <FormCard>
+                    <FormCardImage>
+                        <ScrollHint />
+                    </FormCardImage>
 
-          <FormCardContent
-            onSubmit={handleSubmit}
-            name="form-sign-up"
-            action="/missrea2018"
-            method="post"
-            enctype="multipart/form-data"
-          >
-            <CardHeader>
-              <span>Регистрация</span>
-              <Logo>
-                <img
-                  alt="Школа выживания 2018"
-                  src={`${process.env.PUBLIC_URL}/img/stamps.svg`}
-                />
-              </Logo>
-            </CardHeader>
-            <Fields>
-              <div>
-                <Label required for="fio">
-                  ФИО
-                </Label>
-                <TextInput
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.fio}
-                  id="fio"
-                  type="text"
-                  name="fio"
-                />
-                <Error>{errors.fio}</Error>
-              </div>
+                    <FormCardContent
+                        onSubmit={handleSubmit}
+                        name="form-sign-up"
+                        action="/missrea2018"
+                        method="post"
+                        enctype="multipart/form-data"
+                    >
+                        <CardHeader>
+                            <span>Регистрация</span>
+                            <Logo>
+                                <img
+                                    alt="Школа выживания 2018"
+                                    src={`${
+                                        process.env.PUBLIC_URL
+                                    }/img/stamps.svg`}
+                                />
+                            </Logo>
+                        </CardHeader>
+                        <Fields>
+                            <div>
+                                <Label required for="fio">
+                                    ФИО
+                                </Label>
+                                <TextInput
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.fio}
+                                    id="fio"
+                                    type="text"
+                                    name="fio"
+                                />
+                                <Error>{errors.fio}</Error>
+                            </div>
 
-              <Label required for="fakultet">
-                Факультет
-              </Label>
-              <Select name="fakultet">
-                <option style="display:none;" value="unset" selected>
-                  Выбери...
-                </option>
-                <option value="ФФ">ФФ</option>
-                <option value="ФМЭСИ">ФМЭСИ</option>
-                <option value="ГРТСИ">ГРТСИ</option>
-                <option value="МШБиМЭ">МШБиМЭ</option>
-                <option value="Капитаны России">Капитаны России</option>
-                <option value="ФМа">ФМа</option>
-                <option value="ФМе">ФМе</option>
-                <option value="ФЭП">ФЭП</option>
-                <option value="ФЭТТ">ФЭТТ</option>
-                <option value="ИУиСЭП">ИУиСЭП</option>
-                <option value="БШМиП">БШМиП</option>
-                <option value="Integral">Integral</option>
-              </Select>
+                            <Label required for="fakultet">
+                                Факультет
+                            </Label>
+                            <Select name="fakultet">
+                                <option
+                                    style="display:none;"
+                                    value="unset"
+                                    selected
+                                >
+                                    Выбери...
+                                </option>
+                                <option value="ФФ">ФФ</option>
+                                <option value="ФМЭСИ">ФМЭСИ</option>
+                                <option value="ГРТСИ">ГРТСИ</option>
+                                <option value="МШБиМЭ">МШБиМЭ</option>
+                                <option value="Капитаны России">
+                                    Капитаны России
+                                </option>
+                                <option value="ФМа">ФМа</option>
+                                <option value="ФМе">ФМе</option>
+                                <option value="ФЭП">ФЭП</option>
+                                <option value="ФЭТТ">ФЭТТ</option>
+                                <option value="ИУиСЭП">ИУиСЭП</option>
+                                <option value="БШМиП">БШМиП</option>
+                                <option value="Integral">Integral</option>
+                            </Select>
 
-              <Label required for="kurs">
-                Курс
-              </Label>
-              <Select required name="kurs">
-                <option value="unset" selected style="display:none;">
-                  Выбери...
-                </option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="1 магистратура">1, магистратура</option>
-                <option value="2 магистратура">2, магистратура</option>
-              </Select>
+                            <Label required for="kurs">
+                                Курс
+                            </Label>
+                            <Select required name="kurs">
+                                <option
+                                    value="unset"
+                                    selected
+                                    style="display:none;"
+                                >
+                                    Выбери...
+                                </option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="1 магистратура">
+                                    1, магистратура
+                                </option>
+                                <option value="2 магистратура">
+                                    2, магистратура
+                                </option>
+                            </Select>
 
-              <Label required for="group">
-                Группа
-              </Label>
-              <TextInput id="group" type="text" name="group" />
+                            <Label required for="group">
+                                Группа
+                            </Label>
+                            <TextInput
+                                id="group"
+                                type="text"
+                                name="group"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.group}
+                            />
+                            <Error>{errors.group}</Error>
 
-              <Label required for="phone">
-                Телефон
-              </Label>
-              <PhoneInput id="phone" type="tel" name="phone" />
+                            <Label required for="phone">
+                                Телефон
+                            </Label>
+                            <PhoneInput
+                                id="phone"
+                                type="tel"
+                                name="phone"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.phone}
+                            />
 
-              <Label required for="vkLink">
-                Страница ВКонтакте
-              </Label>
-              <TextInput id="vkLink" type="url" name="vkLink" />
+                            <Error>{errors.phone}</Error>
+                            <Label required for="vkLink">
+                                Страница ВКонтакте
+                            </Label>
+                            <TextInput
+                                id="vkLink"
+                                type="url"
+                                name="vkLink"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.vkLink}
+                            />
 
-              <Label required for="experience">
-                Опыт походов
-              </Label>
-              <Textarea
-                id="experience"
-                name="experience"
-                placeholder="Сколько раз, куда, условия"
-              />
+                            <Error>{errors.vkLink}</Error>
 
-              <Label required for="otherExperience">
-                Другой полезный опыт
-              </Label>
-              <Textarea
-                id="otherExperience"
-                name="otherExperience"
-                placeholder="Страйкбол, спортивное ориентирование и т.д."
-              />
+                            <Label required for="experience">
+                                Опыт походов
+                            </Label>
+                            <Textarea
+                                id="experience"
+                                name="experience"
+                                placeholder="Сколько раз, куда, условия"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.experience}
+                            />
 
-              <Label required for="inventoryAvailiable">
-                Имеющийся инвентарь
-              </Label>
-              <Textarea
-                id="inventoryAvailiable"
-                name="inventoryAvailiable"
-                placeholder="Палатка, спальник, пенка и т.д."
-              />
-              <Label required for="fitnessLevel">
-                Уровень физической подготовки (1-10)
-              </Label>
+                            <Error>{errors.experience}</Error>
 
-              <Select required name="fitnessLevel">
-                <option value="unset" selected style="display:none;">
-                  Выбери...
-                </option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </Select>
+                            <Label required for="otherExperience">
+                                Другой полезный опыт
+                            </Label>
+                            <Textarea
+                                id="otherExperience"
+                                name="otherExperience"
+                                placeholder="Страйкбол, спортивное ориентирование и т.д."
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.otherExperience}
+                            />
 
-              <Label required for="diseases">
-                Болезни и травмы
-              </Label>
-              <Textarea id="diseases" name="diseases" />
+                            <Error>{errors.otherExperience}</Error>
 
-              <Label required for="allergies">
-                Аллергии
-              </Label>
-              <Textarea id="allergies" name="allergies" />
+                            <Label required for="inventoryAvailiable">
+                                Имеющийся инвентарь
+                            </Label>
+                            <Textarea
+                                id="inventoryAvailiable"
+                                name="inventoryAvailiable"
+                                placeholder="Палатка, спальник, пенка и т.д."
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.inventoryAvailiable}
+                            />
 
-              <Label required for="yourTrip">
-                Твое самое дальнее путешествие
-              </Label>
-              <Textarea id="yourTrip" name="yourTrip" />
+                            <Error>{errors.inventoryAvailiable}</Error>
+                            <Label required for="fitnessLevel">
+                                Уровень физической подготовки (1-10)
+                            </Label>
 
-              <Submit type="submit">Отправить</Submit>
-              <Disclamer>
-                * нажимая "Отправить" ты соглашаешься на обработку персональных
-                данных
-              </Disclamer>
-            </Fields>
-          </FormCardContent>
-        </FormCard>
-      </RootContainer>
-    );
-  }
+                            <Select required name="fitnessLevel">
+                                <option
+                                    value="unset"
+                                    selected
+                                    style="display:none;"
+                                >
+                                    Выбери...
+                                </option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </Select>
+
+                            <Label required for="diseases">
+                                Болезни и травмы
+                            </Label>
+                            <Textarea
+                                id="diseases"
+                                name="diseases"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.diseases}
+                            />
+
+                            <Error>{errors.diseases}</Error>
+
+                            <Label required for="allergies">
+                                Аллергии
+                            </Label>
+                            <Textarea
+                                id="allergies"
+                                name="allergies"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.allergies}
+                            />
+
+                            <Error>{errors.allergies}</Error>
+
+                            <Label required for="yourTrip">
+                                Твое самое дальнее путешествие
+                            </Label>
+                            <Textarea
+                                id="yourTrip"
+                                name="yourTrip"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.yourTrip}
+                            />
+
+                            <Error>{errors.yourTrip}</Error>
+
+                            <Submit type="submit">Отправить</Submit>
+                            <Disclamer>
+                                * нажимая "Отправить" ты соглашаешься на
+                                обработку персональных данных
+                            </Disclamer>
+                        </Fields>
+                    </FormCardContent>
+                </FormCard>
+            </RootContainer>
+        );
+    }
 }
 
 export default withFormik({
-  mapPropsToValues: () => ({
-    fio: '',
-  }),
+    mapPropsToValues: () => ({
+        fio: '',
+        group: '',
+        phone: '',
+        vkLink: '',
+        experience: '',
+        otherExperience: '',
+        inventoryAvailiable: '',
+        diseases: '',
+        allergies: '',
+        yourTrip: ''
+    }),
+    validateOnBlur: true,
+    validate: (values, props) => {
+        const errors = {};
+        Object.entries(values).forEach(([field, value]) => {
+            if (!value) {
+                errors[field] = 'Это поле обязательно для заполнения';
+            }
+        });
 
-  validate: (values, props) => {
-    const errors = {};
+        console.log(values, errors);
 
-    Object.entries(values).forEach(([field, value]) => {
-      if (!value) {
-        errors[field] = 'Это поле обязательно для заполнения';
-      }
-    });
+        return errors;
+    },
 
-    console.log(values, errors);
-
-    return errors;
-  },
-
-  handleSubmit: values => {
-    console.log('Значения', values);
-  }
+    handleSubmit: values => {
+        console.log('Значения', values);
+    }
 })(App);
