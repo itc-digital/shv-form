57a55304874707f8c588cfe0b7aad21f2d3871d9
Dmitry Lekomtsev <dolf99@yandex.ru>
2018-03-18 15:33:53 +0400

    Вернул Дисклеймер

--- src/Disclamer.js
+++ src/Disclamer.js
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
-const Disclamer = styled.p`
+const Disclamer = styled.p`   
+    font-family: 'PT Mono', mono;		
     font-size: 12px;
+    font-size: 17.5px;		    
     color #555;
+    color #614A40;		   
     padding: 0 20px;
+    text-align: center;		
+	
+    @media (max-width: 1140px) {		
+        font-size: 20px;		
+  }
 `;
 
 export default Disclamer;
