9ae1b8c1ebd01dd8b9d1646e5e76a94630a9f909
Dmitry Lekomtsev <dolf99@yandex.ru>
2018-03-18 15:39:19 +0400

    Поправил косяк

--- src/Disclamer.js
+++ src/Disclamer.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
 
 const Disclamer = styled.p`   
-    font-family: 'PT Mono', mono;		
-    font-size: 12px;
-    font-size: 17.5px;		    
-    color #555;
-    color #614A40;		   
-    padding: 0 20px;
+    font-family: 'PT Mono', mono;
+    font-size: 17.5px;	
+    color #614A40;		
     text-align: center;		
 	
     @media (max-width: 1140px) {		
